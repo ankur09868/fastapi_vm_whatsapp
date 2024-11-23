bf89f2686b1e007fca941696cdcb229bcc4956a8
Ankur <ankury639@gmail.com>
2024-11-23 16:47:43 +0530

    23-11-2024

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from router import groups_details,bot_config as bot,schedule_message as sch_msg,dashboard
+from router import groups_details,bot_config as bot,schedule_message as sch_msg,dashboard,contacts
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -22,6 +22,7 @@ app.include_router(dashboard.dashboard_router)
 app.include_router(groups_details.router,prefix="/group_details")
 app.include_router(bot.bot_config_router,prefix="/bot_details")
 app.include_router(sch_msg.router,prefix="/schedule_message")
+app.include_router(contacts.contactrouter,prefix="/contact")
 
 
 # Application startup event
